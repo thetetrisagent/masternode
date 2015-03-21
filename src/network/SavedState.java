411d86dff088af66a4aed757e8b9280edfe64796
jyzackoh <zackkohjunyang@gmail.com>
2015-03-22 02:20:39 +0800

    Modified SavedState
    Generalized SavedState class to allow more generic saving of states.

--- src/network/SavedState.java
+++ src/network/SavedState.java
@@ -5,23 +5,23 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class SavedState implements Serializable {
 	
-	private double[] mean;
-	private double[] var;
+	private Object mean;
+	private Object var;
 	private int iterations;
 	private int trainingLoop;
 	
-	public SavedState(double[] mean, double[] var, int iterations, int trainingLoop) {
+	public SavedState(Object mean, Object var, int iterations, int trainingLoop) {
 		this.mean = mean;
 		this.var = var;
 		this.iterations = iterations;
 		this.trainingLoop = trainingLoop;
 	}
 	
-	public double[] getMean() {
+	public Object getMean() {
 		return this.mean;
 	}
 	
-	public double[] getVar() {
+	public Object getVar() {
 		return this.var;
 	}
 	
